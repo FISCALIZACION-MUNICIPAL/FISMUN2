ce0e383de1f7df495a3cd22b5320e72a6b52fdac
Mario Cortez <mrcortez566@gmail.com>
2026-02-25 20:56:07 -0300

    Primer commit: Se cargo los paquetes iniciales del proyecto

---
+++ src/test/java/com/laslajitas/fiscalizacion/FiscalizacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laslajitas.fiscalizacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiscalizacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
